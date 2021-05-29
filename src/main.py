344e53562327a7cf6d382fb5f667ec17a0ee6e5e
kijko <kijkomaciej@gmail.com>
2021-05-29 20:42:04 +0200

    impl first step toward csv product parsing

--- src/main.py
+++ src/main.py
@@ -4,24 +4,8 @@ import gui
 import dbgen
 # todo main-prod/main-dev
 
-def generate_big_data():
-    available_products = [
-        products.Product(1, "FRIDGE-1", 4999.99, "FRIDGE"),
-        products.Product(2, "FRIDGE-2", 2499.89, "FRIDGE"),
-        products.Product(3, "TV-1", 699.89, "TV"),
-        products.Product(4, "TV-2", 6129.00, "TV"),
-        products.Product(5, "TV-3", 8192.99, "TV"),
-        products.Product(6, "X-ONE GAMEPAD", 299.00, "WIRELESS-GAMEPAD"),
-        products.Product(7, "Duracell AAA", 19.99, "AAA-BATTERIES"),
-        products.Product(8, "Coffee maker 2000", 3999.99, "COFFEE-MACHINE"),
-        products.Product(9, "Lavazza 500g", 24.89, "COFFEE"),
-        products.Product(10, "iPhone 15S", 799.99, "PHONE"),
-        products.Product(11, "iPhone special phone charger", 299.00, "PHONE-CHARGER"),
-        products.Product(12, "iPhone USB Cable", 89.00, "PHONE-CABLE"),
-        products.Product(13, "Netflix subscribtion", 39.99, "STREAMING-SERVICE-SUB"),
-        products.Product(14, "Whirpool super washer", 599.90, "WASHING_MACHINE")
-    ]
 
+def generate_big_data():
     date_bonuses = [DateProbabilityBonus(10, 4, 15, 4, 2021, 10, 2), DateProbabilityBonus(5, 5, 5, 5, 2021, 4, 5)]
 
     needs_associations = [
@@ -45,8 +29,8 @@ def generate_big_data():
 
     profiles = [poor_guys_profile, middle_class_profile, rich_man_profile]
 
-    db = dbgen.Database(available_products)
-    in_mem_product_repository = products.ProductRepository(available_products)
+    in_mem_product_repository = products.CSVInMemoryProductRepository("csv-file-path-here!")
+    db = dbgen.Database(in_mem_product_repository.find_all())
 
     example_simulation = Matrix(
         Configuration(global_settings, profiles, in_mem_product_repository),
--- src/products.py
+++ src/products.py
@@ -1,11 +1,29 @@
 
-class ProductRepository:
-    def __init__(self, products):
-        self._products = products
+class CSVInMemoryProductRepository:
+    def __init__(self, csv_file_path):
+        self._products = [
+            Product(1, "FRIDGE-1", 4999.99, "FRIDGE"),
+            Product(2, "FRIDGE-2", 2499.89, "FRIDGE"),
+            Product(3, "TV-1", 699.89, "TV"),
+            Product(4, "TV-2", 6129.00, "TV"),
+            Product(5, "TV-3", 8192.99, "TV"),
+            Product(6, "X-ONE GAMEPAD", 299.00, "WIRELESS-GAMEPAD"),
+            Product(7, "Duracell AAA", 19.99, "AAA-BATTERIES"),
+            Product(8, "Coffee maker 2000", 3999.99, "COFFEE-MACHINE"),
+            Product(9, "Lavazza 500g", 24.89, "COFFEE"),
+            Product(10, "iPhone 15S", 799.99, "PHONE"),
+            Product(11, "iPhone special phone charger", 299.00, "PHONE-CHARGER"),
+            Product(12, "iPhone USB Cable", 89.00, "PHONE-CABLE"),
+            Product(13, "Netflix subscribtion", 39.99, "STREAMING-SERVICE-SUB"),
+            Product(14, "Whirpool super washer", 599.90, "WASHING_MACHINE")
+        ]
 
     def find_by_category_and_max_price(self, category, max_price):
         list_copy = self._products.copy()
         return list(filter(lambda product: product.category == category and product.price <= max_price, list_copy))
+
+    def find_all(self):
+        return self._products
 
 
 class Product:
