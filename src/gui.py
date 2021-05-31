38224fdd651e5b4636887ba4ccabe10be845f8c4
kijko <kijkomaciej@gmail.com>
2021-05-31 21:10:45 +0200

    impl first version of gui

--- src/gui.py
+++ src/gui.py
@@ -1,12 +1,58 @@
 from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
 
-window = Tk()
 
-window.title("Big Data Gen")
-window.geometry("640x480")
+def prepare_gui(run_simulation):
+    window = Tk()
+    window.title("Big Data Gen")
+
+    mainframe = ttk.Frame(window, padding="10 20 10 20")
+    mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+    window.columnconfigure(0, weight=1)
+    window.rowconfigure(0, weight=1)
+
+    def on_config_load_button():
+        config_path.set(filedialog.askopenfilename())
+
+    def on_products_load_button():
+        products_path.set(filedialog.askopenfilename())
+
+    ttk.Button(mainframe, text="Wczytaj plik konfiguracyjny", command=on_config_load_button) \
+        .grid(column=1, row=0, sticky=W)
+
+    ttk.Button(mainframe, text="Wczytaj plik z produktami", command=on_products_load_button) \
+        .grid(column=1, row=1, sticky=W)
+
+    config_path = StringVar()
+    config_path.set("Nie wybrano pliku")
+    ttk.Label(mainframe, text="Plik konfiguracyjny: ").grid(column=0, row=2, sticky=W)
+    ttk.Label(mainframe, textvariable=config_path).grid(column=1, row=2, sticky=W)
+
+    products_path = StringVar()
+    products_path.set("Nie wybrano pliku")
+    ttk.Label(mainframe, text="Plik z produktami: ").grid(column=0, row=3, sticky=W)
+    ttk.Label(mainframe, textvariable=products_path).grid(column=1, row=3, sticky=W)
+
+    generate_button = ttk.Button(mainframe, text="Generuj bazę", command=run_simulation)
+    generate_button.grid(column=1, row=4, sticky=W)
+
+    ttk.Label(mainframe).grid(column=2, row=0)
+
+    status = StringVar()
+    status.set("Nic nie robię")
+    ttk.Label(mainframe, textvariable=status).grid(column=0, row=5, sticky=W)
+
+    progress = StringVar()
+    progress.set("0 na 0")
+    ttk.Label(mainframe, textvariable=progress).grid(column=0, row=6, sticky=W)
+
+    for child in mainframe.winfo_children():
+        child.grid_configure(padx=5, pady=5)
+
+    return window
 
 
 def run_gui(on_run_func):
-    btn = Button(window, text="Rozpocznij symulacje", command=on_run_func)
-    btn.grid(column=1, row=0)
-    window.mainloop()
+    gui = prepare_gui(on_run_func)
+    gui.mainloop()
