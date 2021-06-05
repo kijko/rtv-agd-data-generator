fade66c5ee5e0d8bf58bc7a0b0cee3a60ec465e9
kijko <kijkomaciej@gmail.com>
2021-06-05 16:28:00 +0200

    move schema sql to code

--- src/dbgen.py
+++ src/dbgen.py
@@ -34,11 +34,11 @@ class Database:
                 print("cursor is closed")
 
     def _init_db(self, products):
-        fd = open("./schema.sql", "r")
-        schema_sql = fd.read()
-        fd.close()
+        # fd = open("./schema.sql", "r")
+        # schema_sql = fd.read()
+        # fd.close()
 
-        schema_commands = schema_sql.split(";")
+        schema_commands = schema.split(";")
 
         for command in schema_commands:
             self._cursor.execute(command)
@@ -141,3 +141,64 @@ class DbDataCollector(MatrixEventHandler):
             self._connection.commit()
         # print("************* Zdarzenie ************ - Koniec życia pełnego konsumpcji i pracy ! RIP " + repr(person))
         # self._connection.commit()
+
+
+schema = """
+
+CREATE TABLE customer
+(
+    id            TEXT    PRIMARY KEY NOT NULL,
+    first_name    TEXT                NOT NULL,
+    last_name     TEXT                NOT NULL,
+    email         TEXT                NOT NULL,
+    password_hash TEXT                NOT NULL,
+    phone_number  TEXT                NOT NULL,
+    created_at    DATETIME            NOT NULL,
+    group_name    TEXT                NOT NULL
+);
+
+CREATE TABLE address
+(
+    customer_id TEXT    PRIMARY KEY NOT NULL,
+    country     TEXT                NOT NULL,
+    city        TEXT                NOT NULL,
+    street      TEXT                NOT NULL,
+    apartment   TEXT                NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customer (id)
+);
+
+CREATE TABLE visit
+(
+    id          TEXT PRIMARY KEY NOT NULL,
+    customer_id TEXT             NOT NULL,
+    visit_at    DATETIME         NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customer (id)
+);
+
+CREATE TABLE customer_order
+(
+    id           TEXT PRIMARY KEY NOT NULL,
+    created_at   DATETIME         NOT NULL,
+    payment_type TEXT             NOT NULL,
+    visit_id     TEXT          NOT NULL,
+    FOREIGN KEY (visit_id) REFERENCES visit (id)
+);
+
+CREATE TABLE product
+(
+    id       INTEGER PRIMARY KEY NOT NULL,
+    name     TEXT                NOT NULL,
+    price    REAL                NOT NULL,
+    category TEXT                NOT NULL
+);
+
+CREATE TABLE ordered_product
+(
+    id         INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    product_id INTEGER             NOT NULL,
+    order_id   TEXT                NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES product (id),
+    FOREIGN KEY (order_id) REFERENCES customer_order (id)
+);
+
+"""
